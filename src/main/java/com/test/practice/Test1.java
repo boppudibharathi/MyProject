4211c49701e20453bd8190a046a3c747c564d8d6
boppu <boppudibharathi@gmail.com>
2020-07-18 17:45:56 -0500

    MyFirst commit my second commit final commit

--- src/main/java/com/test/practice/Test1.java
+++ src/main/java/com/test/practice/Test1.java
@@ -1,7 +1,24 @@
 package com.test.practice;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Test1 {
     public static void main(String[] args) {
+
+        List<String> list= new ArrayList<>();
+        list.add("A");
+        list.add("C");
+        list.add("D");
+        list.add("A");
+//        List<String> ls= list.stream().limit(3).collect(Collectors.toList());
+//        ls.forEach(i-> System.out.println(i));
+
+       List<String> ls1= list.stream().filter(e->e.startsWith("A")).collect(Collectors.toList());
+       ls1.forEach(i-> System.out.println(i));
+
+
 
     }
 }
